0aa13f8910ba308102a8383e02e0ef292312c5e1
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-20 11:46:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function FilterableProductTable({ products }) {
   const [filterText, setFilterText] = useState('');
@@ -40,7 +20,6 @@ function FilterableProductTable({ products }) {
     </div>
   );
 }
-
 
 function ProductCategoryRow({ category }) {
   return (
